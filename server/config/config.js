228541e6819ed5f26c8e51bfc5749c1e868f2445
delAmux <luisdelamo@gmail.com>
2019-07-02 00:51:08 +0100

    ref:#master remove line

--- server/config/config.js
+++ server/config/config.js
@@ -13,5 +13,4 @@ process.env.TOKEN_EXPIRE = 60 * 60 * 24 * 30;
 process.env.TOKEN_SEED = process.env.TOKEN_SEED  || 'developer-seed';
 
 // Data base
-
 process.env.URLDB = process.env.URLDB || 'mongodb://localhost:27017/cafe';

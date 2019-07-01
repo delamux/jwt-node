0da2d80abd34345f92715556f60a29a7ee7e2c6b
delAmux <luisdelamo@gmail.com>
2019-07-01 23:15:10 +0100

    ref:#master remove db

--- server/config/config.js
+++ server/config/config.js
@@ -13,13 +13,6 @@ process.env.TOKEN_EXPIRE = 60 * 60 * 24 * 30;
 process.env.TOKEN_SEED = process.env.TOKEN_SEED  || 'developer-seed';
 
 // Data base
-let urlDB;
 
-if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb://localhost:27017/cafe';
-} else {
-    urlDB = 'mongodb+srv://delamux:eYTq53mw1u2K1LvD@delamux-apps-6ede0.mongodb.net/cafe';
-}
-
-process.env.URLDB = urlDB;
+process.env.URLDB = process.env.URLDB || 'mongodb://localhost:27017/cafe';
 
